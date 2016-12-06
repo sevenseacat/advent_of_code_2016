ecf7b75cb48a37a21e4b947921f97efa3d7713ba
Rebecca Skinner <traybaby@gmail.com>
2016-12-06 15:36:33 +0800

    Day 1 refactor
    * Remove magic and be more explicit, listing out all possible turns
    * Add test for parse_input/1

--- lib/advent/day_1.ex
+++ lib/advent/day_1.ex
@@ -1,5 +1,4 @@
 defmodule Advent.Day1 do
-  @directions [:north, :west, :south, :east]
   @doc """
   iex> Advent.Day1.final_distance("R2, L3")
   5
@@ -59,41 +58,46 @@ defmodule Advent.Day1 do
   end
 
   defp make_move({turn, length}, position, facing) do
-    facing = make_turn(facing, turn)
+    facing = turn(facing, turn)
     position = move(position, facing, length)
     {position, facing}
   end
 
   @doc """
-  iex> Advent.Day1.make_turn(:north, "L")
+  iex> Advent.Day1.turn(:north, "L")
   :east
 
-  iex> Advent.Day1.make_turn(:north, "R")
+  iex> Advent.Day1.turn(:north, "R")
   :west
 
-  iex> Advent.Day1.make_turn(:west, "L")
+  iex> Advent.Day1.turn(:west, "L")
   :north
 
-  iex> Advent.Day1.make_turn(:west, "R")
+  iex> Advent.Day1.turn(:west, "R")
   :south
   """
-  def make_turn(facing, turn) do
-    do_make_turn(@directions, facing, turn)
-  end
-
-  defp do_make_turn(_, facing, "S"), do: facing
-  defp do_make_turn([], _facing, "R"), do: List.first(@directions)
-  defp do_make_turn([], _facing, "L"), do: List.last(@directions)
-  defp do_make_turn([facing, new|_directions], facing, "R"), do: new
-  defp do_make_turn([new, facing|_directions], facing, "L"), do: new
-  defp do_make_turn([_|directions], facing, turn), do: do_make_turn(directions, facing, turn)
+  def turn(:north, "L"), do: :east
+  def turn(:north, "R"), do: :west
+  def turn(:south, "L"), do: :west
+  def turn(:south, "R"), do: :east
+  def turn(:east,  "L"), do: :south
+  def turn(:east,  "R"), do: :north
+  def turn(:west,  "L"), do: :north
+  def turn(:west,  "R"), do: :south
+  def turn(dir,    "S"), do: dir
 
   defp move({x, y}, :north, length), do: {x, y+length}
   defp move({x, y}, :south, length), do: {x, y-length}
   defp move({x, y}, :east,  length), do: {x+length, y}
   defp move({x, y}, :west,  length), do: {x-length, y}
 
-  defp parse_input(input) do
+  @doc """
+  Parse the input string into tuples of actions to take.
+
+  iex> Advent.Day1.parse_input("R5, L5, R5, R3\\n")
+  [{"R", 5}, {"L", 5}, {"R", 5}, {"R", 3}]
+  """
+  def parse_input(input) do
     input
     |> String.trim
     |> String.split(", ")
