3d1763a40de695f5e06f0f4416ff4d52c598e343
Rebecca Skinner <traybaby@gmail.com>
2016-12-06 15:07:09 +0800

    Day 3 part 2 solution
    Implement custom chunking functions to split the same input both horizontally
    and vertically

--- lib/advent/day_3.ex
+++ lib/advent/day_3.ex
@@ -5,18 +5,42 @@ defmodule Advent.Day3 do
   end
 
   @doc """
-  Returns a sorted list of triangle sides, eg. [[1,2,3],[4,5,6],...]
+  Returns a sorted list of triangle sides.
+
+  iex> Advent.Day3.input("3 4 5 6 7 8 9 10 11", &Advent.Day3.horizontal_chunk/1)
+  [[3,4,5], [6,7,8], [9,10,11]]
+
+  iex> Advent.Day3.input("3 4 5 6 7 8 9 10 11", &Advent.Day3.vertical_chunk/1)
+  [[3,6,9], [4,7,10], [5,8,11]]
   """
-  def input do
-    File.read!("test/fixtures/day_3")
+  def input(data, fun) do
+    data
     |> String.trim
-    |> String.split("\n")
-    |> Stream.map(fn(triangle) ->
-        triangle
-        |> String.split
-        |> Stream.map(&(String.to_integer(&1)))
-        |> Enum.sort
-      end)
+    |> String.split
+    |> Enum.map(&(String.to_integer(&1)))
+    |> fun.()
+    |> Enum.map(&(Enum.sort(&1)))
+  end
+
+  def horizontal_chunk(list) do
+    list
+    |> Stream.chunk(3)
+  end
+
+  def vertical_chunk(list) do
+    list
+    |> do_vertical_chunk([[], [], []])
+    |> Enum.chunk(3)
+  end
+
+  defp do_vertical_chunk([], results) do
+    results
+    |> Enum.map(&Enum.reverse/1)
+    |> Enum.concat
+  end
+
+  defp do_vertical_chunk([a, b, c|tail], [as, bs, cs]) do
+    do_vertical_chunk(tail, [[a|as], [b|bs], [c|cs]])
   end
 
   @doc """
