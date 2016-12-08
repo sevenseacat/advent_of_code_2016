1d1fd3f130931460e9b8183f5b9102afa34bb994
Rebecca Skinner <traybaby@gmail.com>
2016-12-08 09:31:33 +0800

    Day 7 part 2 solution

--- lib/advent/day_7.ex
+++ lib/advent/day_7.ex
@@ -5,9 +5,47 @@ defmodule Advent.Day7 do
     |> Enum.count
   end
 
+  def ssl_addresses(input) do
+    input
+    |> Stream.reject(&(!ssl_address?(&1)))
+    |> Enum.count
+  end
+
   def input do
     File.read!("test/fixtures/day_7") |> String.split
   end
+
+  @doc """
+  iex> Advent.Day7.ssl_address?("aba[bab]xyz")
+  true
+
+  iex> Advent.Day7.ssl_address?("xyx[xyx]xyx")
+  false
+
+  iex> Advent.Day7.ssl_address?("aaa[kek]eke")
+  true
+
+  iex> Advent.Day7.ssl_address?("zazbz[bzb]cdb")
+  true
+  """
+  def ssl_address?(address), do: check_ssl_address(String.graphemes(address), address)
+
+  def check_ssl_address([], _string), do: false
+  def check_ssl_address([a, b, a | tail], string) when a >= "a" and a <= "z" and b >= "a" and b <= "z" and a != b do
+    case String.match?(string, ~r/\[\w*#{b}#{a}#{b}\w*\]/) do
+      true -> true
+      false -> check_ssl_address([b, a | tail], string)
+    end
+  end
+
+  # Have hit the start of a hypernet section - skip to the end of it to keep parsing
+  def check_ssl_address([ "[" | tail], string) do
+    tail
+    |> Enum.split_while(&(&1 != "]"))
+    |> elem(1)
+    |> check_ssl_address(string)
+  end
+  def check_ssl_address([_head | tail], string), do: check_ssl_address(tail, string)
 
   @doc """
   iex> Advent.Day7.tls_address?("abba[mnop]qrst")
