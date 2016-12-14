a961d8f978506cb5f2542dd1cd4b693e7254ce73
Rebecca Skinner <traybaby@gmail.com>
2016-12-14 19:00:04 +0800

    Move more functions out to where they should be, with better names

--- lib/advent/day_11/elevator.ex
+++ lib/advent/day_11/elevator.ex
@@ -1,3 +1,22 @@
 defmodule Advent.Day11.Elevator do
-  def movement_positions
+  alias Advent.Day11.State
+
+  @doc """
+  iex> Advent.Day11.Elevator.valid_moves(%Advent.Day11.State{elevator: 1, floors: [
+  ...>  %Advent.Day11.Floor{number: 1}, %Advent.Day11.Floor{number: 2}]})
+  [2]
+
+  iex> Advent.Day11.Elevator.valid_moves(%Advent.Day11.State{elevator: 2, floors: [
+  ...>  %Advent.Day11.Floor{number: 1}, %Advent.Day11.Floor{number: 2}]})
+  [1]
+
+  iex> Advent.Day11.Elevator.valid_moves(%Advent.Day11.State{elevator: 2, floors: [
+  ...>  %Advent.Day11.Floor{number: 1}, %Advent.Day11.Floor{number: 2},
+  ...>  %Advent.Day11.Floor{number: 3}]})
+  [1, 3]
+  """
+  def valid_moves(%State{floors: floors, elevator: elevator}) do
+    [elevator+1, elevator-1]
+    |> Enum.filter(fn(floor) -> Enum.any?(floors, &(&1.number == floor)) end)
+  end
 end
--- lib/advent/day_11/floor.ex
+++ lib/advent/day_11/floor.ex
@@ -13,4 +13,31 @@ defmodule Advent.Day11.Floor do
       false -> Enum.empty?(chips) && Enum.empty?(generators)
     end
   end
+
+  @doc """
+  iex> Advent.Day11.Floor.item_combinations(
+  ...>  %Advent.Day11.Floor{chips: [:t, :u], generators: [:v, :w]}) |> Enum.sort
+  [[:t], [:t, :u], [:t, :v], [:t, :w], [:u], [:u, :v], [:u, :w], [:v], [:v, :w], [:w]]
+  """
+  def item_combinations(floor) do
+    floor
+    |> object_permutations(2)
+    |> Enum.map(&Enum.sort/1)
+    |> Enum.map(fn(list) -> Enum.group_by(list, fn({type, value}) -> type end) end)
+    |> Enum.uniq
+  end
+
+  defp object_permutations(_, 0), do: []
+  defp object_permutations(floor, max_size) do
+    Permutations.of(combine_objects(floor), max_size) ++ object_permutations(floor, max_size-1)
+  end
+
+  @doc """
+  Because the same named object can be either a chip or a generator, when we want to
+  generate permutations, we need to differentiate them somehow - do so by turning each
+  name into a tuple of type and name
+  """
+  defp combine_objects(%Floor{chips: chips, generators: generators}) do
+    Enum.map(chips, &({:chips, &1})) ++ Enum.map(generators, &({:generators, &1}))
+  end
 end
